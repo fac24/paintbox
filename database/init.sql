5f2f0492fb89bcd1edd97cd2d9102fbcce02189b
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-06-27 17:34:13 +0100

    update db schema with arts

--- database/init.sql
+++ database/init.sql
@@ -1,37 +1,35 @@
 BEGIN;
 
-
-DROP TABLE IF EXISTS users, posts CASCADE;
-
-
+DROP TABLE IF EXISTS users, arts CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    username TEXT UNIQUE NOT NULL,
-    -- password TEXT NOT NULL
+    username TEXT UNIQUE NOT NULL
 );
 
-
-CREATE TABLE posts (
- mood TEXT NOT NULL 
-);
-
--- how do we show the posts table?
-
-
+CREATE TABLE arts (
+    id SERIAL PRIMARY KEY, 
+    mood TEXT NOT NULL, 
+    alt TEXT NOT NULL,
+    caption TEXT NOT NULL,
+    public BOOLEAN,
+    prompt BOOLEAN,
+    img BYTEA
+    );
 
 INSERT INTO users (email, username) VALUES (
     '1@1.1',
     'ArtistNr1'
-    -- '123'
 );
 
-
-INSERT INTO posts (mood) VALUES 
-('happy');
-
-
-
+INSERT INTO arts (mood, alt, caption, public, prompt, img) VALUES (
+    'Sad',
+    'alttext',
+    'Im sad',
+    TRUE,
+    FALSE,
+    '\x123456'
+);
 
 COMMIT;
