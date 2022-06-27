ce4031ea4fc1a207dcbec27db949cfd85bc8a32f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-06-27 16:42:47 +0100

    start of creating posts table

--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users, posts CASCADE;
 
 
 
@@ -9,14 +9,29 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    -- password TEXT NOT NULL
 );
 
-INSERT INTO users (email, username, password) VALUES (
+
+CREATE TABLE posts (
+ mood TEXT NOT NULL 
+);
+
+-- how do we show the posts table?
+
+
+
+INSERT INTO users (email, username) VALUES (
     '1@1.1',
     'ArtistNr1'
-    '123'
+    -- '123'
 );
+
+
+INSERT INTO posts (mood) VALUES 
+('happy');
+
+
 
 
 COMMIT;
