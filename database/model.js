ff1430debccb47ef79560a5ea53cc3f73f2a8f03
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-06-28 17:12:39 +0100

    get form input data

--- database/model.js
+++ database/model.js
@@ -1,1 +1,25 @@
 import db from "./connection";
+
+async function addArt(mood, alt, caption, pub, prompt, img) {
+  const INSERT_POST = `
+      INSERT INTO arts (
+        mood, 
+        alt,
+        caption,
+        public,
+        prompt ,
+        img) 
+    VALUES ($1, $2, $3, $4, $5, $6)
+    `;
+  const addedArt = await db.query(INSERT_POST, [
+    mood,
+    alt,
+    caption,
+    pub,
+    prompt,
+    img,
+  ]);
+  return addedArt.rows[0];
+}
+
+export { addArt };
--- pages/upload.js
+++ pages/upload.js
@@ -1,14 +1,18 @@
 import UploadForm from "../components/upload/UploadForm";
 
 function Upload() {
+  async function addDataHandler(inputs) {
+    console.log(inputs);
+  }
+
   return (
     <section>
       <h2>Upload your art and share your feelings!</h2>
       <p>
-        You can share your creations and your feellings with the community or
-        you can store them in your MentArt Journal.
+        You can share your creations and your feelings with the community or you
+        can store them in your MentArt Journal.
       </p>
-      <UploadForm />
+      <UploadForm addData={addDataHandler} />
     </section>
   );
 }
