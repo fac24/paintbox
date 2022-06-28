49c43d10a5997be8435d8e2f84fcbf8279b58de5
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-06-28 23:44:51 +0100

    created component for section element

---
+++ components/styled-components/ArtShareEmotionUpload.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ArtShareEmotionUpload = styled.section`
+  //   background-color: green;
+  display: flex;
+  //   flex-direction: coloumn;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 40rem;
+`;
+
+export default ArtShareEmotionUpload;
