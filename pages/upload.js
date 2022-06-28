f3cc95c1ccd51c67bde74eaaa9c5be560e16438f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-06-28 23:45:59 +0100

    replacing section with artshareemotionupload component

--- pages/upload.js
+++ pages/upload.js
@@ -1,20 +1,27 @@
 import StyledDiv from "../components/styled-components/StyledDiv";
 import StyledSubmitButton from "../components/styled-components/StyledSubmitButton";
+import ArtShareEmotionUpload from "../components/styled-components/ArtShareEmotionUpload";
 import UploadForm from "../components/upload/UploadForm";
 
 function Upload() {
   return (
-    <section>
+    // <section>
+    <ArtShareEmotionUpload>
       <h2>Upload your art and share your feelings!</h2>
       <p>
         You can share your creations and your feellings with the community or
         you can store them in your MentArt Journal.
       </p>
+      {/* </ArtShareEmotionUpload> */}
+      {/* container div for the upload form wrapping over the other components */}
       <StyledDiv>
+        {/* upload page component */}
         <UploadForm />
+        {/* styled component button */}
         <StyledSubmitButton>Submit</StyledSubmitButton>
       </StyledDiv>
-    </section>
+    </ArtShareEmotionUpload>
+    // </section>
   );
 }
 
