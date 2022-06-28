dd37f073ee293bda9bc926c17d562be7d6c49c18
vasystus <artgalla8@gmail.com>
2022-06-28 17:13:17 +0100

    prompt expanding box created

---
+++ components/prompt/PromptWord.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaUpload } from "react-icons/fa";
+
+
+function PromptWord() {
+  const [open, setOpen] = React.useState(false);
+  function toggleOpen() {
+    setOpen(!open);
+  }
+
+  const moreInfo = !open ? null : (
+    <div>
+      <p>
+        If you’re lack of inspiration or like challenging yourself our weekly
+        prompt gives you a word to create your next art work based on it.
+      </p>
+      <p>
+        There are no rules, you can interpret the word according to your own
+        feelings and mood.
+      </p>
+      <p>
+        Upload your creation and store it in your Mentart Journal or share it
+        with the public whenever you’re ready to talk about it.
+      </p>
+    </div>
+  );
+  return (
+    <div>
+      <h2>Weekly Prompt</h2>
+      <h3>Prompt word goes here</h3>
+      <FaUpload />
+      <button onClick={toggleOpen}>
+        More info
+      </button>
+      {moreInfo}
+    </div>
+  );
+}
+
+export default PromptWord;
