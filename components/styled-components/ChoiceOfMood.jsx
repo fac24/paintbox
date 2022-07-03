0c4c36f7c75c9b9592a94b722feeba3faf6186f3
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 20:02:45 +0100

    mood choice text from the user and it's styling

--- components/styled-components/ChoiceOfMood.jsx
+++ components/styled-components/ChoiceOfMood.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const ChoiceOfMood = styled.h3`
-  font-size: 1.0625rem;
-  font-weight: 700;
-  text-transform: none;
-  line-height: 1.4;
+  font-size: 1.75rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  line-height: 1;
   color: #784890;
   margin: 0px;
 `;
