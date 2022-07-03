8d8a919237ea494307b0427072e9704432cace6a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 11:51:47 +0100

    image styled component for journal page

---
+++ components/styled-components/JournalImage.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const JournalImage = styled.img`
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  height: fit-content;
+  border-radius: 16px;
+  border-width: 4px;
+  cursor: pointer;
+`;
+
+export default JournalImage;
