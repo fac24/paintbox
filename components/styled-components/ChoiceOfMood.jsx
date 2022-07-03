706ada525dbfc9f0f6ceb374aa6703fdb8d1f8f8
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 12:28:56 +0100

    description displayed on mood component

---
+++ components/styled-components/ChoiceOfMood.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ChoiceOfMood = styled.h3`
+  font-size: 1.0625rem;
+  font-weight: 700;
+  text-transform: none;
+  line-height: 1.4;
+  color: #784890;
+  margin: 0px;
+`;
+
+export default ChoiceOfMood;
