a2a0709d6a912bad2b79248a76e020990da34dbc
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 20:05:03 +0100

    modified with the appropriate positions of components for each style and placed the component paths within the journal feed file

--- components/journal/JournalFeed.js
+++ components/journal/JournalFeed.js
@@ -1,8 +1,13 @@
 import { useRouter } from "next/router";
+// import DescriptionContainer from "../styled-components/DescriptionContainer";
 // import ArtUploadImage from "../styled-components/ArtUploadImage";
+// import WholeJournalWrapper from "../styled-components/WholeJournalWrapper";
 import ChoiceOfMood from "../styled-components/ChoiceOfMood";
 import JournalImage from "../styled-components/JournalImage";
 import MentartWrapper from "../styled-components/MentartWrapper";
+// import MentartWrapper from "../styled-components/MentartWrapper";
+import MoodPost from "../styled-components/MoodPost";
+import RainbowBorder from "../styled-components/RainbowBorder";
 
 function JournalFeed(props) {
   const router = useRouter();
@@ -10,20 +15,29 @@ function JournalFeed(props) {
   const allArts = props.arts;
 
   return (
-    <MentartWrapper>
+    <div>
       {allArts.map((art) => {
         const href = `/posts/${art.id}`;
         return (
-          <li key={art.id}>
-            <ChoiceOfMood>{art.mood}</ChoiceOfMood>
-            <p>{art.date}</p>
-            <JournalImage src={art.img} alt={art.alt} />
-            <p>{art.caption}</p>
-            <button onClick={() => router.push(href)}>Open...</button>
-          </li>
+          <>
+            <RainbowBorder>
+              <MoodPost key={art.id}>
+                <p>
+                  {art.date}
+                  <JournalImage src={art.img} alt={art.alt} />
+                </p>
+                <li>
+                  {/* <DescriptionContainer key={art.id}> */}
+                  <ChoiceOfMood>{art.mood}</ChoiceOfMood>
+                  <p>{art.caption}</p>
+                  <button onClick={() => router.push(href)}>Open...</button>
+                </li>
+              </MoodPost>
+            </RainbowBorder>
+          </>
         );
       })}
-    </MentartWrapper>
+    </div>
   );
 }
 
