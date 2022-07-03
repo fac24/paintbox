f4467398395562fa5e606a062108b4bf8a29fcda
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 19:57:59 +0100

    styling for ul element on mood component

--- components/styled-components/MoodPost.jsx
+++ components/styled-components/MoodPost.jsx
@@ -1,24 +1,15 @@
 import styled from "styled-components";
 
-const MoodPost = styled.div`
+const MoodPost = styled.ul`
   display: grid;
-  gap: 1.5rem;
-  grid-template-columns: repeat(2, 1fr);
-  padding: 3.5rem 0px;
-  border-top: 4px solid rgba(255, 255, 255, 0.2);
-
-  &:first-of-type {
-    border-top: none;
-    padding-top: 0;
-  }
-
-  &:last-of-type {
-    padding-bottom: 0;
-  }
-
-  @media only screen and (max-width: 835px) {
-    grid-template-columns: 1fr;
-  }
+  padding: 2rem;
+  list-style-type: none;
+  grid-template-columns: 300px 1fr;
+  gap: 1rem;
+  align-items: center;
+  max-width: 800px;
+  margin: 3rem;
+  font: 500 100%/1.5 system-ui;
 `;
 
 export default MoodPost;
