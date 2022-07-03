13662ab9c4be320143e9a31b1ded09672f4b040c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 19:30:39 +0100

    component style section added

---
+++ components/styled-components/WholeJournalWrapper.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const WholeJournalWrapper = styled.section`
+  //   margin-left: auto;
+  //   margin-right: auto;
+  //   max-width: 40rem;
+  display: flex;
+  //   flex-direction: column;
+  //   justify-content: center;
+  //   align-items: center;
+  //   gap: 50px;
+  //   padding: 10vw 1rem;
+
+  section > * {
+    flex-basis: 100%;
+  }
+`;
+
+export default WholeJournalWrapper;
