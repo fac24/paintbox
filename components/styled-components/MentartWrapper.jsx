5336cafedaeea696d771583068823c2c572cdb23
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 22:51:33 +0100

    stored and commented code

--- components/styled-components/MentartWrapper.jsx
+++ components/styled-components/MentartWrapper.jsx
@@ -1,15 +1,18 @@
 // import styled from "styled-components";
 
-// const MentartWrapper = styled.ul`
-//   appearance: none;
-//   background: transparent;
-//   border: 0px;
-//   display: block;
-//   margin: 0px;
-//   min-width: 0px;
-//   padding: 0px;
-//   position: relative;
-//   text-align: inherit;
+// const MentartWrapper = styled.section`
+//   //   appearance: none;
+//   //   background: transparent;
+//   //   border: 0px;
+//   //   display: block;
+//   //   margin: 0px;
+//   //   min-width: 0px;
+//   //   padding: 0px;
+//   //   position: relative;
+//   //   text-align: inherit;
+
+//   background-color: var(--deep-soothing-ocean);
+//   z-index: 1;
 // `;
 
 // export default MentartWrapper;
