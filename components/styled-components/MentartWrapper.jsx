1af9e76e63c40acaf9441c68f1ca2caf359553c7
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 19:59:29 +0100

    styling component to wrap styling

--- components/styled-components/MentartWrapper.jsx
+++ components/styled-components/MentartWrapper.jsx
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
 const MentartWrapper = styled.ul`
-    appearance: none;
-    background: transparent;
-    border: 0px;
-    display: block;
-    margin: 0px;
-    min-width: 0px;
-    padding: 0px;
-    position: relative;
-    text-align: inherit;
-}
+  appearance: none;
+  background: transparent;
+  border: 0px;
+  display: block;
+  margin: 0px;
+  min-width: 0px;
+  padding: 0px;
+  position: relative;
+  text-align: inherit;
 `;
 
 export default MentartWrapper;
