81e6f99e456b1d111f8c14aee14538e070f86216
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 12:15:51 +0100

    added more styling elements

--- components/styled-components/MoodPost.jsx
+++ components/styled-components/MoodPost.jsx
@@ -6,6 +6,19 @@ const MoodPost = styled.div`
   grid-template-columns: repeat(2, 1fr);
   padding: 3.5rem 0px;
   border-top: 4px solid rgba(255, 255, 255, 0.2);
+
+  &:first-of-type {
+    border-top: none;
+    padding-top: 0;
+  }
+
+  &:last-of-type {
+    padding-bottom: 0;
+  }
+
+  @media only screen and (max-width: 835px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export default MoodPost;
