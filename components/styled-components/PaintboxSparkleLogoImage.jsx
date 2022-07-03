474ec9c2d73716e53e2db03ef348d6d596ab28f0
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-04 00:46:23 +0100

    style component for paintbox logo

---
+++ components/styled-components/PaintboxSparkleLogoImage.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const PaintboxSparkleLogoImage = styled.img`
+  display: flex;
+  //   flex-wrap: wrap;
+  flex-direction: column;
+  width: 50%;
+  height: 50%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default PaintboxSparkleLogoImage;
