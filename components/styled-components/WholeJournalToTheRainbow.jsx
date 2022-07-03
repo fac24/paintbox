8a91f10753e4e75f13e3224669c45a56c271b698
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 19:45:36 +0100

    styled component created for title of mentart journal

---
+++ components/styled-components/WholeJournalToTheRainbow.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const WholeJournalToTheRainbow = styled.h2`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 40rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 0.5rem;
+`;
+
+export default WholeJournalToTheRainbow;
