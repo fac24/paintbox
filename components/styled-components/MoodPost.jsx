a1725f02a19716b836f36cc967277e9dad058226
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-03 11:52:28 +0100

    mood post container styling

---
+++ components/styled-components/MoodPost.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const MoodPost = styled.div`
+  display: grid;
+  gap: 1.5rem;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 3.5rem 0px;
+  border-top: 4px solid rgba(255, 255, 255, 0.2);
+`;
+
+export default MoodPost;
