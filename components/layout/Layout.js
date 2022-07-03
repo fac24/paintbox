26b7ff0fc3a0d1e11401a0ff54f35a4fca56a02e
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-04 00:45:39 +0100

    added paintbox logo

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,8 +1,15 @@
 import Nav from "./Nav";
+import PaintboxSparkleLogoImage from "../styled-components/PaintboxSparkleLogoImage";
+// import Image from "next/image";
+// import paintbox from "public/Sparkle-paintbox.png";
 
 function Layout(props) {
   return (
     <div>
+      <PaintboxSparkleLogoImage
+        src="/Sparkle-paintbox.png"
+        alt="paintbox-logo"
+      />
       <header>
         <h1>Paintbox</h1>
         <Nav />
