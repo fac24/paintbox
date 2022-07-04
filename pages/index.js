95f09309eed67a4a79bfd4344b7e7dee9c6fa9ab
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-07-04 08:51:16 +0100

    set session provider, add login btn

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import { signIn } from "next-auth/react";
 import Affirmation from "../components/affirmation/Affirmation";
 
 function Landing() {
   return (
     <div>
+      <section>
+        <button onClick={signIn}>Login</button>
+      </section>
       <Affirmation />
     </div>
   );
