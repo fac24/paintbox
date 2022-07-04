90550ec6b80dbc5fcc48f04a487a7006bdb0cda7
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-04 01:45:27 +0100

    added padding and flex wrap to the painbox logo style component

--- components/styled-components/PaintboxSparkleLogoImage.jsx
+++ components/styled-components/PaintboxSparkleLogoImage.jsx
@@ -2,12 +2,16 @@ import styled from "styled-components";
 
 const PaintboxSparkleLogoImage = styled.img`
   display: flex;
-  //   flex-wrap: wrap;
-  flex-direction: column;
-  width: 50%;
-  height: 50%;
+  flex-wrap: wrap;
+  //   flex-direction: column;
+  width: 100%;
+  height: auto%;
   align-items: center;
   justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 40rem;
+  padding: 5%;
 `;
 
 export default PaintboxSparkleLogoImage;
