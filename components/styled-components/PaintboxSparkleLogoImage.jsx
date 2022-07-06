47cce3606257acc191c45e7fdd954c5521e8b282
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-06 17:30:14 +0100

    removed commented out code

--- components/styled-components/PaintboxSparkleLogoImage.jsx
+++ components/styled-components/PaintboxSparkleLogoImage.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const PaintboxSparkleLogoImage = styled.img`
   display: flex;
   flex-wrap: wrap;
-  //   flex-direction: column;
   width: 100%;
   height: auto%;
   align-items: center;
