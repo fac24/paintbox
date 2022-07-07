47ecc39dbeccb8edc910f72a99190dfb7818814c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-07 01:52:17 +0100

    updated style div component

--- components/styled-components/StyledDiv.jsx
+++ components/styled-components/StyledDiv.jsx
@@ -5,7 +5,9 @@ const StyledDiv = styled.div`
   flex-wrap: nowrap;
   justify-content: center;
   align-items: center;
-  background-color: pink;
+  background-color: var(--lightly-white-pink);
+  border: 5px solid var(--deep-soothing-ocean);
+  border-radius: 10px;
   margin-left: auto;
   margin-right: auto;
   max-width: 40rem;
