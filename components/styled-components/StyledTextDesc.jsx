4231841c3e0e8499c589dea80f12b2063545305e
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-08 00:53:43 +0100

    created new h4 tag styled component

---
+++ components/styled-components/StyledTextDesc.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledTextDesc = styled.h4`
+  color: var(--aubergine-purple);
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 40rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 0.5rem;
+`;
+
+export default StyledTextDesc;
