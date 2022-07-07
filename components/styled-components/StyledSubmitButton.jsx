fe32d6673a06d9c908f9ffcc99c3e5b77ded3c5b
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-07 01:53:25 +0100

    updated button component

--- components/styled-components/StyledSubmitButton.jsx
+++ components/styled-components/StyledSubmitButton.jsx
@@ -4,12 +4,14 @@ const StyledSubmitButton = styled.button`
   background-color: var(--lilly-lilac);
   color: var(--deep-soothing-ocean);
   display: flex;
-  border-radius: 10px;
   justify-content: center;
   align-items: center;
   margin-left: auto;
   margin-right: auto;
   max-width: 40rem;
+  border-radius: 12px;
+  padding: 1%;
+  margin: 5% auto;
 `;
 
 export default StyledSubmitButton;
