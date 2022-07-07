feacbd791e23341334a7ba3f1c8937b1600a98ba
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-07 22:35:11 +0100

    new gallery container created

---
+++ components/styled-components/GalleryContainer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const GalleryContainer = styled.div`
+  // display: -ms-flexbox; /* IE10 */
+  // display: flex;
+  // -ms-flex-wrap: wrap; /* IE10 */
+  // flex-wrap: wrap;
+  // padding: 0 4px;
+
+  width: 95%;
+  margin: 0.5rem auto;
+`;
+
+export default GalleryContainer;
