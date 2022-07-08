3b8ae1dacfef24f0b0d94c79c52b54509f1b854f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-08 01:59:06 +0100

    styling for drop down toggles and button colour and style

--- components/prompt/PromptWord.js
+++ components/prompt/PromptWord.js
@@ -4,6 +4,9 @@ import { FaCloudUploadAlt } from "react-icons/fa";
 import { FaAngleDown } from "react-icons/fa";
 import WholeJournalToTheRainbowTitle from "../styled-components/WholeJournalToTheRainbowTitle";
 import StyledTextDesc from "../styled-components/StyledTextDesc";
+import StyledDiv from "../styled-components/StyledDiv";
+import styled from "styled-components";
+// import StyledSubmitButton from "../styled-components/StyledSubmitButton";
 
 function PromptWord() {
   const [open, setOpen] = React.useState(false);
@@ -22,7 +25,7 @@ function PromptWord() {
   });
 
   const moreInfo = !open ? null : (
-    <div>
+    <PromptText>
       <p>
         If you’re lack of inspiration or like challenging yourself our weekly
         prompt gives you a word to create your next art work based on it.
@@ -35,7 +38,7 @@ function PromptWord() {
         Upload your creation and store it in your Mentart Journal or share it
         with the public whenever you’re ready to talk about it.
       </p>
-    </div>
+    </PromptText>
   );
   return (
     <div>
@@ -43,18 +46,62 @@ function PromptWord() {
         Weekly Prompt
       </WholeJournalToTheRainbowTitle>
       <StyledTextDesc>Prompt word goes here</StyledTextDesc>
-
-      {/* upload button */}
-      <Link href="/upload" passHref>
-        <RedirectButton />
-      </Link>
-      {/* expanding text button */}
-      <button onClick={toggleOpen}>
-        <FaAngleDown />
-      </button>
-      {moreInfo}
+      <StyledDivPrompt>
+        {/* upload button */}
+        <StyledLink>
+          <Link href="/upload" passHref>
+            <RedirectButton />
+          </Link>
+        </StyledLink>
+        {/* expanding text button */}
+        <Button onClick={toggleOpen}>
+          <FaAngleDown />
+        </Button>
+        {moreInfo}
+      </StyledDivPrompt>
     </div>
   );
 }
 
 export default PromptWord;
+
+const StyledDivPrompt = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--lightly-white-pink);
+  border: 5px solid var(--deep-soothing-ocean);
+  border-radius: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 40rem;
+`;
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1rem;
+  margin: 1rem;
+  padding: 0.25em 1rem;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const StyledLink = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1rem;
+  margin: 1rem;
+  padding: 0.25em 1rem;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const PromptText = styled.div`
+  text-align: center;
+`;
